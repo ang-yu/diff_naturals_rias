5753b8b90aa76bb119800df3ca3464581a4b103a
angyu <35616220+ang-yu@users.noreply.github.com>
2024-06-26 17:27:16 -0500

    Update simulation.R

--- simulation.R
+++ simulation.R
@@ -1,41 +1,46 @@
 
 library(ggplot2)
+library(ggpubr)
 
-sim <- function(v) {
-  set.seed(102)
-  A <- rbinom(100000, 1, 0.5)
+logit <- function (x) { sapply(x, function (y) {rbinom(1,1,exp(y)/(1+exp(y)))} ) }
+
+sim <- function(b) {
+  set.seed(1)
   
-  L_epsilon <- rnorm(100000, 0, v)
+  L_epsilon <- rnorm(100000, 0, 1)
   L_0 <- L_epsilon
   L_1 <- 1 + L_epsilon
   
   M_epsilon <- rnorm(100000, 0, 1)
-  M_0 <- L_0 + M_epsilon
-  M_1 <- -1 + L_1 + cos(L_1) + M_epsilon
+  M_0 <- logit(L_0 + M_epsilon)
+  M_1 <- logit(1 + L_1 + b*L_1 + M_epsilon)
   G_0 <- sample(M_0, 100000, replace = FALSE)
   G_1 <- sample(M_1, 100000, replace = FALSE)
   
   Y_epsilon <- rnorm(100000, 0, 1)
   Y_1M_1 <- 1 + L_1 + M_1 + L_1*M_1 + Y_epsilon
   Y_1M_0 <- 1 + L_1 + M_0 + L_1*M_0 + Y_epsilon
+  Y_0M_0 <- L_0 + M_1 + L_0*M_1 + Y_epsilon
   
   Y_1G_1 <- 1 + L_1 + G_1 + L_1*G_1 + Y_epsilon
   Y_1G_0 <- 1 + L_1 + G_0 + L_1*G_0 + Y_epsilon
+  Y_0G_0 <- L_0 + G_0 + L_0*G_0 + Y_epsilon
   
   NIE <- mean(Y_1M_1-Y_1M_0)
   NIER <- mean(Y_1G_1-Y_1G_0)
   
-  return(c(NIE, NIER))
+  NDE <- mean(Y_1M_0-Y_0M_0)
+  NDER <- mean(Y_1G_0-Y_0G_0)
+  
+  return(c(NIE, NIER, NDE, NDER))
 }
 
-parameter_seq <- seq(0, 5, 0.001)
+parameter_seq <- seq(-3, 0, 0.01)
 results <- as.data.frame( t(sapply(parameter_seq, sim)) )
 results$p <- parameter_seq
-colnames(results)[1:2] <- c("NIE", "NIER")
+colnames(results)[1:4] <- c("NIE", "NIER", "NDE", "NDER")
 
-shading_data <- results[which(results$NIE < 0)[1]:4946,]
-
-figure <- ggplot(data = results, aes(x = p)) +
+figure_ie <- ggplot(data = results, aes(x = p)) +
   geom_line(aes(y = NIE, colour = "NIE", linetype = "NIE")) +  
   geom_line(aes(y = NIER, colour = "NIER", linetype = "NIER")) +  
   labs(title = "Plot of y = NIE and y = NIER", 
@@ -45,12 +50,24 @@ figure <- ggplot(data = results, aes(x = p)) +
   scale_linetype_manual(values = c("NIE" = "solid", "NIER" = "dashed"),
                         name = "Estimands", labels = c("NIE", "NIER")) +
   theme_minimal() +
-  geom_ribbon(data=shading_data, aes(ymin=pmin(NIE, NIER), ymax=pmax(NIE, NIER)), fill = "gray", alpha = 1) +
-  geom_hline(yintercept=0, color="black") +
-  #annotate("point", x = results$p[which(results$NIE < 0)[1]], y = results$NIER[which(results$NIE < 0)[1]], color = "black", size = 2) +
-  labs(title = NULL, x = "Parameter value", y = NULL) +
-  theme(plot.background = element_rect(fill = "white", colour = "white")) +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank())
+  geom_ribbon(data=results[which(results$NIE < 0 & results$NIER > 0),], aes(ymin=pmin(NIE, NIER), ymax=pmax(NIE, NIER)), fill = "gray", alpha = 1) +
+  labs(title = NULL, x = "b value", y = NULL) +
+  theme(plot.background = element_rect(fill = "white", colour = "white")) 
 
 
+figure_de <- ggplot(data = results, aes(x = p)) +
+  geom_line(aes(y = NDE, colour = "NDE", linetype = "NDE")) +  
+  geom_line(aes(y = NDER, colour = "NDER", linetype = "NDER")) +  
+  labs(title = "Plot of y = NDE and y = NDER", 
+       x = "X axis", y = "Y axis") +
+  scale_colour_manual(values = c("NDE" = "blue", "NDER" = "red"),
+                      name = "Estimands", labels = c("NDE", "NDER")) +
+  scale_linetype_manual(values = c("NDE" = "solid", "NDER" = "dashed"),
+                        name = "Estimands", labels = c("NDE", "NDER")) +
+  theme_minimal() +
+  labs(title = NULL, x = "b value", y = NULL) +
+  theme(plot.background = element_rect(fill = "white", colour = "white")) 
+
+ggarrange(figure_ie, figure_de,
+          labels = c("(a)", "(b)"),
+          ncol = 2, nrow = 1)
